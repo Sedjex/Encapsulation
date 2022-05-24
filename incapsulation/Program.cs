52e86fcd93161ed91a4066811499f968cdc2205b
Sedjex <kathshem@gmail.com>
2022-05-24 17:22:23 +0300

    first commit

--- incapsulation/Program.cs
+++ incapsulation/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 
-namespace incapsulation
+namespace Encapsulation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Renderer renderer = new Renderer();
+            Player player = new Player(5, 5);
+
+            renderer.DrawPlayer(player.X, player.Y);
+        }
+    }
+
+    class Renderer
+    {
+        public void DrawPlayer(int x, int y, char ch = '@')
+        {
+            Console.SetCursorPosition(x,y);
+            Console.Write(ch);
+            Console.ReadKey();
+        }
+    }
+
+    class Player
+    {
+
+        public int X { get; private set; }
+
+        public int Y { get; private set; }
+
+        public Player (int x, int y)
+        {
+            Y = y;
+            X = x;
         }
     }
 }
+
